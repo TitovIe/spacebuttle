6dbfbd315f786a14c3bcb9ae8fd7c11858f268e4
Ilya <titov.ie@phystech.edu>
2023-11-25 18:03:38 +0300

    Initial commit

---
+++ src/main/java/ru/otus/spacebuttle/SpacebuttleApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spacebuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacebuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacebuttleApplication.class, args);
+	}
+
+}
